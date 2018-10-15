ae17a28a80fb9fad2fc7839baa3a8c7910c900ce
wumh1610 <353342361@qq.com>
2018-10-15 23:13:00 +0800

    commit get configuration from lua

---
+++ src/config.lua
@@ -0,0 +1,7 @@
+
+
+ip = "127.0.0.1"
+port = 3306
+userName = "root"
+password = "123456"
+sid = "dongnaobike"
--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,67 @@
 
 #include <functional>
 
+#include <string.h>
+extern "C"
+{
+    #include "lauxlib.h"  
+    #include "lualib.h"  
+    #include "lua.h"
+}
+typedef struct conf{
+    char ip[10];
+    int port;
+    char userName[20];
+    char password[20];
+    char sid[20];
+}Conf;
+
+Conf config;
+
+void getConfig(){
+	
+	lua_State* L;
+    //initialize Lua
+    L = luaL_newstate();
+    //load Lua base libraries
+    luaL_openlibs(L);
+    //load the script
+    if(luaL_loadfile(L, "fun.lua") || lua_pcall(L, 0 , 0, 0)){
+        cout << "dofile error " << lua_tostring(L, -1);
+        exit;
+    }
+    memset(&config, 0, sizeof(Conf));
+	//get configure information
+    lua_getglobal(L, "ip");
+    if(!lua_isstring(L, -1)){
+            cout << "ip is not string" << endl;
+    }
+    lua_getglobal(L, "port");
+    if(!lua_isnumber(L, -1)){
+            cout << "port is not number" << endl;
+    }
+    lua_getglobal(L, "userName");
+    if(!lua_isstring(L, -1)){
+            cout << "userName is not string" << endl;
+    }
+    lua_getglobal(L, "password");
+    if(!lua_isstring(L, -1)){
+            cout << "password is not string" << endl;
+    }
+    lua_getglobal(L, "sid");
+    if(!lua_isstring(L, -1)){
+            cout << "sid is not string" << endl;
+    }
+
+    strcpy(config.sid, lua_tostring(L, -1));
+    strcpy(config.password, lua_tostring(L, -2));
+    strcpy(config.userName, lua_tostring(L, -3));
+    config.port = lua_tonumber(L, -4);
+    strcpy(config.ip, lua_tostring(L, -5));
+	
+	lua_close(L);
+}
+
 int main(int argc, char** argv)
 {
     if (argc != 2)
@@ -29,7 +90,10 @@ int main(int argc, char** argv)
     dms->open();
     
     std::shared_ptr<MysqlConnection> mysqlconn(new MysqlConnection);
-    mysqlconn->Init("127.0.0.1", 3306, "root", "123456", "dongnaobike");
+//    mysqlconn->Init("127.0.0.1", 3306, "root", "123456", "dongnaobike");
+	//get information from lua
+	getConfig();
+    mysqlconn->Init(config.ip, config.port, config.userName, config.password, config.sid);
     
     BusinessProcessor processor(dms, mysqlconn);
     processor.init();
